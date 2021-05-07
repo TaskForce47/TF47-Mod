1a106d4d4cd098650830b28280252a8aea9f3e13
Oliver Krüppel <krueppel@cs.uni-bonn.de>
2021-05-07 17:48:51 +0200

    nerf sa7

---
+++ addons/vt_sa7_patch/config.cpp
@@ -0,0 +1,14 @@
+#include "script_component.hpp"
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {};
+        author = "TF47 Dragon";
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgAmmo.hpp"
