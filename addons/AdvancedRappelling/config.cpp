a6f4a2fc8134e35452320d0256251f7952cdd93d
DeRampage <git@philipp-loebel.de>
2021-05-14 19:47:05 +0200

    minor stuff

--- addons/AdvancedRappelling/config.cpp
+++ addons/AdvancedRappelling/config.cpp
@@ -10,19 +10,16 @@ class CfgPatches
 
 class CfgNetworkMessages
 {
-   
    class AdvancedRappellingRemoteExecClient
    {
       module = "AdvancedRappelling";
       parameters[] = {"ARRAY","STRING","OBJECT","BOOL"};
    };
-   
    class AdvancedRappellingRemoteExecServer
    {
       module = "AdvancedRappelling";
       parameters[] = {"ARRAY","STRING","BOOL"};
    };
-   
 };
 
 class CfgFunctions 
