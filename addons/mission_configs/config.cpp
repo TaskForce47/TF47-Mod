01d8ff2a762186b87504e1b2eea6b22990f4080d
Oliver Krüppel <krueppel@cs.uni-bonn.de>
2021-05-21 22:16:28 +0200

    added description config from old corescripts

---
+++ addons/mission_configs/config.cpp
@@ -0,0 +1,15 @@
+#include "script_component.hpp"
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {};
+        author = "TF47 Dragon";
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgDebriefing.hpp"
+#include "CfgNotifications.hpp"
