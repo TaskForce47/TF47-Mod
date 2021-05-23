51c067e5eac3207b8998b2dd2b077b83957729d8
DeRampage <git@philipp-loebel.de>
2021-05-23 22:31:08 +0200

    hello?

---
+++ addons/main/script_mod.hpp
@@ -0,0 +1,11 @@
+// COMPONENT should be defined in the script_component.hpp and included BEFORE this hpp
+
+#define MAINPREFIX z
+#define PREFIX TF47
+
+#include "script_version.hpp"
+
+#define VERSION MAJOR.MINOR.PATCH.BUILD
+#define VERSION_AR MAJOR,MINOR,PATCH,BUILD
+
+#define REQUIRED_VERSION 2.02
