2d79ceeeff25ee7c2c20610abb0c8b92ecf39382
Desty <patrick.tarnok+tf47git@gmail.com>
2021-05-30 13:17:46 +0200

    tabs...

--- addons/G36_mag_fix/config.cpp
+++ addons/G36_mag_fix/config.cpp
@@ -19,24 +19,24 @@ class CfgMagazines {
     class rhssaf_30rnd_556x45_EPR_G36: 30Rnd_556x45_Stanag {
         mass =  0.9;
     };
-	
-	class rhssaf_30rnd_556x45_MDIM_G36: 30Rnd_556x45_Stanag {
+    
+    class rhssaf_30rnd_556x45_MDIM_G36: 30Rnd_556x45_Stanag {
         mass =  0.9;
     };
-	
-	class rhssaf_30rnd_556x45_SOST_G36: 30Rnd_556x45_Stanag {
+    
+    class rhssaf_30rnd_556x45_SOST_G36: 30Rnd_556x45_Stanag {
         mass =  0.9;
     };
-	
-	class rhssaf_30rnd_556x45_SPR_G36: 30Rnd_556x45_Stanag {
+    
+    class rhssaf_30rnd_556x45_SPR_G36: 30Rnd_556x45_Stanag {
         mass =  0.9;
     };
-	
-	class rhssaf_30rnd_556x45_TDIM_G36: 30Rnd_556x45_Stanag {
+    
+    class rhssaf_30rnd_556x45_TDIM_G36: 30Rnd_556x45_Stanag {
         mass =  0.9;
     };
-	
-	class rhssaf_30rnd_556x45_Tracers_G36: 30Rnd_556x45_Stanag {
+    
+    class rhssaf_30rnd_556x45_Tracers_G36: 30Rnd_556x45_Stanag {
         mass =  0.9;
     };
 };
