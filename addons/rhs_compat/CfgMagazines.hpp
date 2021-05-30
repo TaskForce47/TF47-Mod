49cd433130cdf1aa0861514b42a7680f89ff4524
Desty <patrick.tarnok+tf47git@gmail.com>
2021-05-30 17:19:54 +0200

    rhs compas

--- addons/rhs_compat/CfgMagazines.hpp
+++ addons/rhs_compat/CfgMagazines.hpp
@@ -1,16 +1,3 @@
-#include "script_component.hpp"
-class CfgPatches {
-    class ADDON {
-        name = COMPONENT;
-        units[] = {};
-        weapons[] = {};
-        requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"A3_Weapons_F","rhssaf_c_weapons","rhssaf_m_weapon_g36"};
-        author = "TF47 ra.Y";
-        VERSION_CONFIG;
-    };
-};
-
 class CfgMagazines {
     //base class
     class 30Rnd_556x45_Stanag;
