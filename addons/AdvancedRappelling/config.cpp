41960bc03ee991b5b0cd2f58ace433f55fee5628
DeRampage <git@philipp-loebel.de>
2021-06-01 22:01:41 +0200

    final fix for animations

--- addons/AdvancedRappelling/config.cpp
+++ addons/AdvancedRappelling/config.cpp
@@ -39,19 +39,19 @@ class CfgSounds
    class AR_Rappel_Loop
    {
       name = "AR_Rappel_Loop";
-      sound[] = {"\AR_AdvancedRappelling\sounds\AR_Rappel_Loop.ogg", db+20, 1};
+      sound[] = {"\z\TF47\addons\AdvancedRappelling\sounds\AR_Rappel_Loop.ogg", db+20, 1};
       titles[] = {0,""};
    };
    class AR_Rappel_Start
    {
       name = "AR_Rappel_Start";
-      sound[] = {"\AR_AdvancedRappelling\sounds\AR_Rappel_Start.ogg", db+20, 1};
+      sound[] = {"\z\TF47\addons\sounds\AR_Rappel_Start.ogg", db+20, 1};
       titles[] = {0,""};
    };
    class AR_Rappel_End
    {
       name = "AR_Rappel_End";
-      sound[] = {"\AR_AdvancedRappelling\sounds\AR_Rappel_End.ogg", db+20, 1};
+      sound[] = {"\z\TF47\addons\AdvancedRappelling\sounds\AR_Rappel_End.ogg", db+20, 1};
       titles[] = {0,""};
    };
 };
