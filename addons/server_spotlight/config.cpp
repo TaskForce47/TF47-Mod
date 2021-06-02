dde5422ee57977efd1f0d01c5571582d538fe247
Desty <patrick.tarnok+tf47git@gmail.com>
2021-06-03 01:05:30 +0200

    server spotlight inital

---
+++ addons/server_spotlight/config.cpp
@@ -0,0 +1,31 @@
+class CfgPatches {
+    class server_spotlight {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 0.6;
+        requiredAddons[] = {"A3_Weapons_F"};
+        version = 2.0;
+        versionStr = 2.0;
+        versionAr[] = {2, 0};
+        author = "TF47 Desty";
+    };
+};
+
+class RscStandardDisplay;
+class RscDisplayMain: RscStandardDisplay
+{
+	class Spotlight
+	{
+		class AwesomeServer
+		{
+			text = "JOIN [TF47] MilSim Public #1"; // Text displayed on the square button, converted to upper-case
+			textIsQuote = 0; // 1 to add quotation marks around the text
+			picture = "\server_spotlight\button.paa"; // Square picture, ideally 512x512  --- RAY MACH SCHNELLER
+			//video = "\a3\Ui_f\Video\spotlight_1_Apex.ogv"; // Video played on mouse hover --- RAY MACH SCHNELLER
+			//action = "0 = [_this, 'your.domain.name', '2302', 'yourpasshere'] execVM '\server_spotlight\joinServer.sqf';";
+			action = "0 = [_this, '195.201.81.83', '2302', ''] execVM '\server_spotlight\joinServer.sqf';";
+			actionText = "JOIN [TF47] MilSim Public #1"; // Text displayed in top left corner of on-hover white frame
+			condition = "true"; // Condition for showing the spotlight
+		};
+	};
+};
