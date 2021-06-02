1182d03ad2e1913a7934b43c5c148fb8a8bb6d3b
Desty <patrick.tarnok+tf47git@gmail.com>
2021-06-02 23:36:53 +0200

    marco renaming missing ...only in death, atonement.

--- addons/vehicle_medical/config_macros.hpp
+++ addons/vehicle_medical/config_macros.hpp
@@ -1,6 +1,6 @@
-#define ADDON Vehicle_Medical
-#define ADDON_NAME Ace Vehicle Medical
-#define FUNC(name) ADDON##_fnc_##name
-#define ICON_PATH(name) z\TF47\addons\ADDON##\ui\##name##.paa
+#define ADDON TF47
+#define ADDON_NAME TF47_VehicleMedical
+#define FUNC(name) ADDON##_fn_##name
+#define ICON_PATH(name) z\TF47\addons\vehicle_medical\ui\##name##.paa
 #define QUOTE(target) #target
 #define CONCAT(a,b) a##b
