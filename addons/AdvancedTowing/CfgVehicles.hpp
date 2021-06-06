a59d04794f0da2e7e3335dc407c1ead2c598e57c
DeRampage <git@philipp-loebel.de>
2021-06-06 20:42:31 +0200

    prepare own Towing Hook

---
+++ addons/AdvancedTowing/CfgVehicles.hpp
@@ -0,0 +1,8 @@
+class CfgVehicles {
+    class Land_Can_V2_F;
+    class TF47_towing_hook: Land_Can_V2_F {
+        author = "TF47 Rampage";
+        model = "\a3\data_f\Hook\Hook_F.p3d";
+        displayName = "Towing Hook";
+    };
+};
