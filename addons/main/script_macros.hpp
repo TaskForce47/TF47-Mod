62da1d5999c46df232975126be24cd68f657df1d
DeRampage <git@philipp-loebel.de>
2021-06-06 14:14:05 +0200

    reduce rpt errors @Desty

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -1,5 +1,7 @@
 #include "\z\ace\addons\main\script_macros.hpp"
 #include "\x\cba\addons\main\script_macros_common.hpp"
+#include "\x\cba\addons\xeh\script_xeh.hpp"
+
 #define DFUNC(var1) TRIPLES(ADDON,fnc,var1)
 #ifdef DISABLE_COMPILE_CACHE
     #undef PREP
