487fc6e636425562df1abb2e1712bab95a0b7e40
Desty <patrick.tarnok+tf47git@gmail.com>
2021-06-16 18:43:53 +0200

    menuvid / logo

--- addons/server_spotlight/config.cpp
+++ addons/server_spotlight/config.cpp
@@ -20,8 +20,8 @@ class RscDisplayMain: RscStandardDisplay
         {
             text = "JOIN [TF47] MilSim Public #1"; // Text displayed on the square button, converted to upper-case
             textIsQuote = 0; // 1 to add quotation marks around the text
-            picture = "z\TF47\addons\server_spotlight\button.paa"; // Square picture, ideally 512x512  --- RAY MACH SCHNELLER
-            //video = "\a3\Ui_f\Video\spotlight_1_Apex.ogv"; // Video played on mouse hover --- RAY MACH SCHNELLER
+            picture = "z\TF47\addons\server_spotlight\tf47_patch_ger.paa"; // Square picture, ideally 512x512  --- RAY MACH SCHNELLER
+            video = "z\TF47\addons\server_spotlight\menuvid.ogv"; // Video played on mouse hover --- RAY MACH SCHNELLER
             //action = "0 = [_this, 'your.domain.name', '2302', 'yourpasshere'] execVM '\server_spotlight\joinServer.sqf';";
             action = "0 = [_this, '195.201.81.83', '2302', ''] execVM 'z\TF47\addons\server_spotlight\joinServer.sqf';";
             actionText = "JOIN [TF47] MilSim Public #1"; // Text displayed in top left corner of on-hover white frame
