4fe85e8bb1439d6036a006096c2d3aa2f08aef52
Desty <patrick.tarnok+tf47git@gmail.com>
2021-06-26 17:46:12 +0200

    fix paths for macros

--- addons/TFARSetter/script_component.hpp
+++ addons/TFARSetter/script_component.hpp
@@ -1,5 +1,5 @@
 #define COMPONENT TFARSetter
-#include "\z\TF47\addons\main\script_mod.hpp"
+#include "\z\TF47\addons\TFARSetter\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
@@ -14,4 +14,4 @@
     #define DEBUG_SETTINGS DEBUG_SETTINGS_TFAR_ACE_EXTENDED
 #endif
 
-#include "\z\TF47\addons\main\script_macros.hpp"
+#include "\z\TF47\addons\TFARSetter\script_macros.hpp"
