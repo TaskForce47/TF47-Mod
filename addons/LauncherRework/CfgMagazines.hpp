87a2177dfaf0fce5c5b9bac61809877255021965
raY <sebastian.schneider12@gmail.com>
2021-07-02 00:00:46 +0200

    MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMunition

--- addons/LauncherRework/CfgMagazines.hpp
+++ addons/LauncherRework/CfgMagazines.hpp
@@ -6,7 +6,7 @@ class CfgMagazines {
         scope = 2;
         displayName = "FFV756 MT (Multi Target)";
         model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
-        picture = "\rhsusf\addons\rhsusf_weapons2\m3maaws\UI\gear_small_CA.paa";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_MT.paa";
         initSpeed = 290;
         descriptionShort = "Anti Building";
         displaynameshort = "756 MT";
@@ -20,7 +20,7 @@ class CfgMagazines {
         scope = 2;
         displayName = "FFV751 HEAT (Tandem)";
         model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
-        picture = "\rhsusf\addons\rhsusf_weapons2\m3maaws\UI\gear_small_CA.paa";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_HEAT_TD.paa";
         initSpeed = 290;
         descriptionShort = "High Explosive Anti tank";
         displaynameshort = "751 HEAT";
@@ -34,7 +34,7 @@ class CfgMagazines {
         scope = 2;
         displayName = "FFV655 HEAT (CS)";
         model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
-        picture = "\rhsusf\addons\rhsusf_weapons2\m3maaws\UI\gear_small_CA.paa";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_HEAT_CS.paa";
         initSpeed = 290;
         descriptionShort = "High Explosive Anti tank CS";
         displaynameshort = "655 HEAT CS";
@@ -48,7 +48,7 @@ class CfgMagazines {
         scope = 2;
         displayName = "FFV551C HEAT RS";
         model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
-        picture = "\rhsusf\addons\rhsusf_weapons2\m3maaws\UI\gear_small_CA.paa";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_HEAT_RS.paa";
         initSpeed = 290;
         descriptionShort = "High Explosive Anti tank RS";
         displaynameshort = "551C HEAT";
@@ -62,7 +62,7 @@ class CfgMagazines {
         scope = 2;
         displayName = "FFV551 HEAT";
         model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
-        picture = "\rhsusf\addons\rhsusf_weapons2\m3maaws\UI\gear_small_CA.paa";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_HEAT.paa";
         initSpeed = 290;
         descriptionShort = "High Explosive Anti tank";
         displaynameshort = "551 HEAT";
@@ -70,88 +70,244 @@ class CfgMagazines {
         type = 2*256;
         mass = 70.05; //3,2 kg
     };
-    
+    // ------------Fuckup Line-----------------------------------------------------------------
     class tf47_mag_maaws_ffv545c_illum : CA_LauncherMagazine
 	{
         scope = 2;
-        displayName = "FFV751 HEAT (Tandem)";
+        displayName = "FFV545 ILLUM";
         model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
-        picture = "\rhsusf\addons\rhsusf_weapons2\m3maaws\UI\gear_small_CA.paa";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_ILLUM.paa";
         initSpeed = 290;
-        descriptionShort = "High Explosive Anti tank";
-        displaynameshort = "751 HEAT";
-        ammo = tf47_ammo_raws_ffv751_heat;
+        descriptionShort = "Illum";
+        displaynameshort = "545 ILLUM";
+        ammo = tf47_ammo_raws_ffv545_illum;
         type = 2*256;
-        mass = 88.20; //4 kg
+        mass = 68.34; //3,1 kg
     };
     
     class tf47_mag_maaws_ffv509_asm : CA_LauncherMagazine
 	{
         scope = 2;
-        displayName = "FFV751 HEAT (Tandem)";
+        displayName = "FFV509 ASM";
         model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
-        picture = "\rhsusf\addons\rhsusf_weapons2\m3maaws\UI\gear_small_CA.paa";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_ASM.paa";
         initSpeed = 290;
-        descriptionShort = "High Explosive Anti tank";
-        displaynameshort = "751 HEAT";
-        ammo = tf47_ammo_raws_ffv751_heat;
+        descriptionShort = "Anti Structure Munition";
+        displaynameshort = "509 ASM";
+        ammo = tf47_ammo_raws_ffv509_asm;
         type = 2*256;
-        mass = 88.20; //4 kg
+        mass = 92.59; //4,2 kg
     };
     
     class tf47_mag_maaws_ffv502_hedp : CA_LauncherMagazine
 	{
         scope = 2;
-        displayName = "FFV751 HEAT (Tandem)";
+        displayName = "FFV502 HEDP";
         model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
-        picture = "\rhsusf\addons\rhsusf_weapons2\m3maaws\UI\gear_small_CA.paa";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_HEDP.paa";
         initSpeed = 290;
-        descriptionShort = "High Explosive Anti tank";
-        displaynameshort = "751 HEAT";
-        ammo = tf47_ammo_raws_ffv751_heat;
+        descriptionShort = "High Explosive Dual Purpose";
+        displaynameshort = "502 HEDP";
+        ammo = tf47_ammo_raws_ffv502_hedp;
         type = 2*256;
-        mass = 88.20; //4 kg
+        mass = 72.75; //3,3 kg
     };
     
     class tf47_mag_maaws_ffv469c_smoke : CA_LauncherMagazine
 	{
         scope = 2;
-        displayName = "FFV751 HEAT (Tandem)";
+        displayName = "FFV469C SMOKE";
         model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
-        picture = "\rhsusf\addons\rhsusf_weapons2\m3maaws\UI\gear_small_CA.paa";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_WEED.paa";
         initSpeed = 290;
-        descriptionShort = "High Explosive Anti tank";
-        displaynameshort = "751 HEAT";
-        ammo = tf47_ammo_raws_ffv751_heat;
+        descriptionShort = "Smoke";
+        displaynameshort = "469C SMOKE";
+        ammo = tf47_ammo_raws_ffv469c_smoke;
         type = 2*256;
-        mass = 88.20; //4 kg
+        mass = 68.34; //3,1 kg
     };
     
-    class tf47_mag_maaws_ffv441d_hedp : CA_LauncherMagazine
+    class tf47_mag_maaws_ffv441d_he : CA_LauncherMagazine //<<< Classname war vorher x_ffv441d_hedp - Typo? Im CfgWeapons isses nur HE.
 	{
         scope = 2;
-        displayName = "FFV751 HEAT (Tandem)";
+        displayName = "FFV441D HE";
         model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
-        picture = "\rhsusf\addons\rhsusf_weapons2\m3maaws\UI\gear_small_CA.paa";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_HE.paa";
         initSpeed = 290;
-        descriptionShort = "High Explosive Anti tank";
-        displaynameshort = "751 HEAT";
-        ammo = tf47_ammo_raws_ffv751_heat;
+        descriptionShort = "High Explosive";
+        displaynameshort = "441D HE";
+        ammo = tf47_ammo_raws_ffv441d_he;
         type = 2*256;
-        mass = 88.20; //4 kg
+        mass = 70.54; //3,2 kg
     };
     
     class tf47_mag_maaws_ffv401_adm : CA_LauncherMagazine
 	{
         scope = 2;
+        displayName = "FFV401 ADM (FLECHETTE)";
+        model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_ADM.paa";
+        initSpeed = 290;
+        descriptionShort = "Area Defence Munition";
+        displaynameshort = "401 ADM";
+        ammo = tf47_ammo_raws_ffv401_adm;
+        type = 2*256;
+        mass = 59.52; //2,7 kg
+    };
+	
+	//RAWS
+	
+class tf47_mag_raws_ffv756_mt : CA_LauncherMagazine
+	{
+        scope = 2;
+        displayName = "FFV756 MT (Multi Target)";
+        model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_MT.paa";
+        initSpeed = 350;
+        descriptionShort = "Anti Building";
+        displaynameshort = "756 MT";
+        ammo = tf47_ammo_raws_ffv756_mt;
+        type = 2*256;
+        mass = 97.02; //4,4 kg
+    };
+    
+	class tf47_mag_raws_ffv751_heat : CA_LauncherMagazine
+	{
+        scope = 2;
         displayName = "FFV751 HEAT (Tandem)";
         model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
-        picture = "\rhsusf\addons\rhsusf_weapons2\m3maaws\UI\gear_small_CA.paa";
-        initSpeed = 290;
+        picture = "z\TF47\addons\LauncherRework\ui\M3_HEAT_TD.paa";
+        initSpeed = 350;
         descriptionShort = "High Explosive Anti tank";
         displaynameshort = "751 HEAT";
         ammo = tf47_ammo_raws_ffv751_heat;
         type = 2*256;
         mass = 88.20; //4 kg
+    };
+    
+    class tf47_mag_raws_ffv655_heat_cs : CA_LauncherMagazine
+	{
+        scope = 2;
+        displayName = "FFV655 HEAT (CS)";
+        model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_HEAT_CS.paa";
+        initSpeed = 350;
+        descriptionShort = "High Explosive Anti tank CS";
+        displaynameshort = "655 HEAT CS";
+        ammo = tf47_ammo_raws_ffv655_heat_cs;
+        type = 2*256;
+        mass = 105.82; //4,8 kg
+    };
+    
+    class tf47_mag_raws_ffv551c_heat : CA_LauncherMagazine
+	{
+        scope = 2;
+        displayName = "FFV551C HEAT RS";
+        model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_HEAT_RS.paa";
+        initSpeed = 350;
+        descriptionShort = "High Explosive Anti tank RS";
+        displaynameshort = "551C HEAT";
+        ammo = tf47_ammo_raws_ffv551c_heat;
+        type = 2*256;
+        mass = 77.17; //3,5 kg
+    };
+    
+    class tf47_mag_raws_ffv551_heat : CA_LauncherMagazine
+	{
+        scope = 2;
+        displayName = "FFV551 HEAT";
+        model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_HEAT.paa";
+        initSpeed = 350;
+        descriptionShort = "High Explosive Anti tank";
+        displaynameshort = "551 HEAT";
+        ammo = tf47_ammo_raws_ffv551_heat;
+        type = 2*256;
+        mass = 70.05; //3,2 kg
+    };
+
+    class tf47_mag_raws_ffv545c_illum : CA_LauncherMagazine
+	{
+        scope = 2;
+        displayName = "FFV545 ILLUM";
+        model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_ILLUM.paa";
+        initSpeed = 350;
+        descriptionShort = "Illum";
+        displaynameshort = "545 ILLUM";
+        ammo = tf47_ammo_raws_ffv545_illum;
+        type = 2*256;
+        mass = 68.34; //3,1 kg
+    };
+    
+    class tf47_mag_raws_ffv509_asm : CA_LauncherMagazine
+	{
+        scope = 2;
+        displayName = "FFV509 ASM";
+        model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_ASM.paa";
+        initSpeed = 350;
+        descriptionShort = "Anti Structure Munition";
+        displaynameshort = "509 ASM";
+        ammo = tf47_ammo_raws_ffv509_asm;
+        type = 2*256;
+        mass = 92.59; //4,2 kg
+    };
+    
+    class tf47_mag_raws_ffv502_hedp : CA_LauncherMagazine
+	{
+        scope = 2;
+        displayName = "FFV502 HEDP";
+        model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_HEDP.paa";
+        initSpeed = 350;
+        descriptionShort = "High Explosive Dual Purpose";
+        displaynameshort = "502 HEDP";
+        ammo = tf47_ammo_raws_ffv502_hedp;
+        type = 2*256;
+        mass = 72.75; //3,3 kg
+    };
+    
+    class tf47_mag_raws_ffv469c_smoke : CA_LauncherMagazine
+	{
+        scope = 2;
+        displayName = "FFV469C SMOKE";
+        model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_WEED.paa";
+        initSpeed = 350;
+        descriptionShort = "Smoke";
+        displaynameshort = "469C SMOKE";
+        ammo = tf47_ammo_raws_ffv469c_smoke;
+        type = 2*256;
+        mass = 68.34; //3,1 kg
+    };
+    
+    class tf47_mag_raws_ffv441d_he : CA_LauncherMagazine
+	{
+        scope = 2;
+        displayName = "FFV441D HE";
+        model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_HE.paa";
+        initSpeed = 350;
+        descriptionShort = "High Explosive";
+        displaynameshort = "441D HE";
+        ammo = tf47_ammo_raws_ffv441d_he;
+        type = 2*256;
+        mass = 70.54; //3,2 kg
+    };
+    
+    class tf47_mag_raws_ffv401_adm : CA_LauncherMagazine
+	{
+        scope = 2;
+        displayName = "FFV401 ADM (FLECHETTE)";
+        model = "rhsusf\addons\rhsusf_weapons2\m3maaws\mag_m3maaws.p3d";
+        picture = "z\TF47\addons\LauncherRework\ui\M3_ADM.paa";
+        initSpeed = 350;
+        descriptionShort = "Area Defence Munition";
+        displaynameshort = "401 ADM";
+        ammo = tf47_ammo_raws_ffv401_adm;
+        type = 2*256;
+        mass = 59.52; //2,7 kg
     };
 };
