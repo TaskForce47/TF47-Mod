fbce85a6548d8d03d4c4e521864b1956ba50e3de
DeRampage <git@philipp-loebel.de>
2023-05-02 13:34:23 +0200

    Test Hemtt

--- meta.cpp
+++
@@ -1,4 +0,0 @@
-protocol = 1;
-publishedid = 2481926480;
-name = "Taskforce47";
-timestamp = 5249247525780833956;
--- mod.cpp
+++ mod.cpp
@@ -1,12 +1,32 @@
-name = "Taskforce47";
-tooltipOwned= "TF47";
-logo = "TF47.paa";
-logoOver = "TF47.paa";
-overviewPicture = "TF47.paa";
-author = "TF47 Dev Team";
-hidePicture = 0;
+/// Name of your mod
+name = "PMC";
+/// Picture displayed from the expansions menu/ Optimal size is 2048x1024, other sizes work too
+picture = "tf47forecon.paa";
+/// Display next to the item added by the mod
+logoSmall = "tf47forecon.paa";
+/// Logo displayed in the main menu
+logo = "tf47forecon.paa";
+/// When the mouse is over, in the main menu
+logoOver = "tf47forecon.paa";
+/// Website URL, that can accessed from the expansions menu
+action = "https://steamcommunity.com/groups/tf47forecon";
+// name of the action button
+actionName = "Find us on Steam";
+/// Tool tip displayed when the mouse is left over, in the main menu
+tooltipOwned = "BAxOPF on!";
+/// Color used for DLC stripes and backgrounds (RGBA)
+dlcColor[] =
+{
+	0.23,
+	0.39,s
+	0.30,
+	1
+};
+/// Overview text, displayed from the extension menu
+overview = "This is the mod for standardized BAxOPF-Gameplay. HF GL. Dont complain.";
+/// Hide the extension name
 hideName = 0;
-actionName = "Website";
-action = "http://taskforce47/";
-description = "Taskforce47 Community Mod";
-overview="Taskforce47 Community Mod";
+/// Hide the extension menu
+hidePicture = 0;
+// author?
+author = "Rampage"";
